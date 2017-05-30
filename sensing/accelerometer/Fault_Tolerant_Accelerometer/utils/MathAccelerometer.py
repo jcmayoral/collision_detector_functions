48414252f1c97a1eed8d963caa5d50229c60d75a
jcmayoral <jocamaba1989@gmail.com>
2017-05-30 12:51:56 +0000

    mean and variance added

--- sensing/accelerometer/Fault_Tolerant_Accelerometer/utils/MathAccelerometer.py
+++ sensing/accelerometer/Fault_Tolerant_Accelerometer/utils/MathAccelerometer.py
@@ -3,16 +3,26 @@
 # License: Public Domain
 import time
 import driver
+import numpy as np
 
 class MathAccelerometer:
+
    def __init__(self):
       print("MathAccelerometer Constructor")
       self.accel = driver.FaultAccelerometer()
+
+   def mean(self,data):
+      return np.mean(data, axis=0)
+
+   def variance(self,data):
+      return np.var(data, axis=0)
+
    def plot(self):
-      print('Printing X, Y, Z axis values, press Ctrl-C to quit...')
       while True:
-         # Read the X, Y, Z axis acceleration values and print them.
-         x, y, z = self.accel.read()
-         print('X={0}, Y={1}, Z={2}'.format(x, y, z))
-         # Wait half a second and repeat.
-         time.sleep(0.5)
+ 	 samples = []
+         for i in range(0,500):
+             samples.append(self.accel.read())
+         mean = self.mean(samples)
+         variance = self.variance(samples)
+         print('Mean Values X={0}, Y={1}, Z={2}'.format(mean[0], mean[1], mean[2]))
+         print('Variance Values X={0}, Y={1}, Z={2}'.format(variance[0], variance[1], variance[2]))
