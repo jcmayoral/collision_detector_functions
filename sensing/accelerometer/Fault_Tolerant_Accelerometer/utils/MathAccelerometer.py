f53a032a5e5fbb3521ef0e37d0ebc72c21d00ac1
jcmayoral <jocamaba1989@gmail.com>
2017-05-30 16:15:00 +0000

    CUSUM added and mean Gaussian Sequence added

--- sensing/accelerometer/Fault_Tolerant_Accelerometer/utils/MathAccelerometer.py
+++ sensing/accelerometer/Fault_Tolerant_Accelerometer/utils/MathAccelerometer.py
@@ -11,6 +11,7 @@ class MathAccelerometer:
       print("MathAccelerometer Constructor")
       self.accel = driver.FaultAccelerometer()
       self.seconds = seconds
+      self.cum_sum = 0
 
    def mean(self,data):
       return np.mean(data, axis=0)
@@ -30,6 +31,8 @@ class MathAccelerometer:
 
    def changeDetection(self):
       print ('time in seconds ', self.accel.get_data_rate())
+      expected_mean = [2,2,]
+      expected_variance = [1,1,1]
       
       while True:   	   
          samples = []
@@ -39,6 +42,23 @@ class MathAccelerometer:
 	     samples.append(self.accel.read())
          mean = self.mean(samples)
          variance = self.variance(samples)
-         print('Mean Values X={0}, Y={1}, Z={2}'.format(mean[0], mean[1], mean[2]))
-         print('Variance Values X={0}, Y={1}, Z={2}'.format(variance[0], variance[1], variance[2]))
+         self.CUSUM(samples,mean,variance, expected_mean, expected_variance)
+         #print('Mean Values X={0}, Y={1}, Z={2}'.format(mean[0], mean[1], mean[2]))
+         #print('Variance Values X={0}, Y={1}, Z={2}'.format(variance[0], variance[1], variance[2]))
 
+   def CUSUM(self, data, mean, var, e_mean, e_var):
+      array = np.array(data)
+      likelihood,s_z = self.meanGaussianSequence(array, mean, var, e_mean)
+      print ('Gaussian Probability Density Function ' , likelihood)
+      print ('log-likelihood ratio', s_z)
+   
+   def meanGaussianSequence(self,z, m1, v1, m0):
+      likelihood = []
+      s_z = []
+      for i in range(0,3):
+         likelihood.append((np.power(-z[-1,i]- m1[i],2) + np.power(-z[-1,i]- m0[i],2))/(2*v1[i]))  
+         s_z.append(((m1[i]-m0[i]) /v1[i])* (z[-1,i] - ((m0[i]+m1[i])/2)))
+
+      likelihood = np.exp(likelihood)
+      
+      return likelihood, s_z
