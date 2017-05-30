905fe75cf81bee83929e6fc1ea210a5c5c5d2a40
jcmayoral <jocamaba1989@gmail.com>
2017-05-30 09:35:30 +0000

    fault tolerant accelerometer init commit

---
+++ sensing/accelerometer/Fault_Tolerant_Accelerometer/README.md
@@ -0,0 +1,2 @@
+This library is based on the Adafruit Python ADXL345 library available at https://github.com/adafruit/Adafruit_Python_ADXL345.git
+
---
+++ sensing/accelerometer/Fault_Tolerant_Accelerometer/setup.py
@@ -0,0 +1,30 @@
+try:
+    # Try using ez_setup to install setuptools if not already installed.
+    from ez_setup import use_setuptools
+    use_setuptools()
+except ImportError:
+    # Ignore import error and assume Python 3 which already has setuptools.
+    pass
+
+from setuptools import setup, find_packages
+
+classifiers = ['Development Status :: 4 - Beta',
+               'Operating System :: POSIX :: Linux',
+               'License :: OSI Approved :: None',
+               'Intended Audience :: Developers',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3',
+               'Topic :: Software Development',
+               'Topic :: System :: Hardware']
+
+setup(name              = 'Fault_Tolerant_Accelerometer',
+      version           = '1.0.0',
+      author            = 'Jose C Mayoral Banos',
+      author_email      = 'jocamaba1989@gmail.com',
+      description       = 'Fault Tolerance Algorithms implemented on a I2C accelerometer in a Raspberry Pi',
+      license           = 'none',
+      classifiers       = classifiers,
+      url               = 'https://github.com/adafruit/Adafruit_Python_ADXL345/',
+      dependency_links  = ['https://github.com/jcmayoral/myRaspberryPI.git'],
+      install_requires  = [''],
+      packages          = find_packages())
