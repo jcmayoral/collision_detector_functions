73163489889c667eb7fe2452420b92a6bdcc86a5
jcmayoral <jocamaba1989@gmail.com>
2017-08-07 12:28:35 +0200

    gauss plot implementing

--- statics/MyStatics/GaussianPlotter.py
+++ statics/MyStatics/GaussianPlotter.py
@@ -1,20 +1,22 @@
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-import matplotlib.mlab as mlab
+from scipy.stats import norm
 import numpy as np
 import math
 
 class GaussPlot:
     def __init__(self):
-        self.f = plt.figure()
-        self.ax = plt.axes()
-        self.ax.set_title('Gaussian Bell')
-        self.ax.legend("True")
-        plt.gca().set_color_cycle(['red', 'green', 'blue'])
-        plt.show()
         print ("GaussiPlotter Constructor Initialized")
 
     def call(self,data,mu,variance):
         sigma = np.sqrt(variance)
-        plotObject = self.ax.plot(data,mlab.normpdf(data, mu, sigma))
+        #plotObject = self.ax.plot(data,mlab.normpdf(data, mu, sigma))
+        #initialize a normal distribution with frozen in mean=-1, std. dev.= 1
+        print ("sigma", sigma)
+        self.rv = []
+        for i in range(len(sigma)):
+            self.rv.append(norm(loc = mu[i], scale = sigma[i]))
+        self.x = np.arange(-10, 10, .1)
+        #plot the pdfs of these normal distributions
+        #plt.plot(x, rv.pdf(x), x, rv1.pdf(x), x, rv2.pdf(x))
