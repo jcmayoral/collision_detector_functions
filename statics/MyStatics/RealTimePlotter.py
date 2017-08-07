3ed111b8e2ca1a85a4940602de6e60ffff9e9a57
jcmayoral <jocamaba1989@gmail.com>
2017-08-07 11:18:28 +0200

    git ignroe and statics added

---
+++ statics/MyStatics/RealTimePlotter.py
@@ -0,0 +1,26 @@
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+class RealTimePlotter:
+    def __init__(self, max_samples = 1000, pace = 200):
+        self.max_samples = max_samples
+        self.f = plt.figure()
+        self.ax = plt.axes()
+        self.ax.set_title('Results')
+        self.ax.legend("True")
+        self.pace_ = pace
+        plt.gca().set_color_cycle(['red', 'green', 'blue'])
+        print ("RealTimePlotter Constructor Initialized")
+
+
+    def update(self,seq,x,y):
+        if seq % self.pace_ is 0:
+            plotObject = self.ax.plot(x,y)
+            plt.legend(iter(plotObject), ('x', 'y', 'z'))
+            plt.draw()
+
+        if len(x) is self.max_samples:
+            x.pop(0)
+            y.pop(0)
+            self.ax.cla()
