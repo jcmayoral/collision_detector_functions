b70da4dfce83da75abe49f275c83eb51fd0e5352
jcmayoral <jocamaba1989@gmail.com>
2017-08-07 13:35:17 +0200

    plotting gaussian of X and comparing gaussians but realtime plot not working properly

--- statics/MyStatics/GaussianPlotter.py
+++ statics/MyStatics/GaussianPlotter.py
@@ -13,10 +13,8 @@ class GaussPlot:
         sigma = np.sqrt(variance)
         #plotObject = self.ax.plot(data,mlab.normpdf(data, mu, sigma))
         #initialize a normal distribution with frozen in mean=-1, std. dev.= 1
-        print ("sigma", sigma)
         self.rv = []
         for i in range(len(sigma)):
             self.rv.append(norm(loc = mu[i], scale = sigma[i]))
-        self.x = np.arange(-10, 10, .1)
         #plot the pdfs of these normal distributions
         #plt.plot(x, rv.pdf(x), x, rv1.pdf(x), x, rv2.pdf(x))
